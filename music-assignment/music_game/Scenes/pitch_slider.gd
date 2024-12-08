0182ac44897fc19197edc257f0b96d31229255bc
y2kpartytime <C24732831@myTUDublin.ie>
2024-12-08 10:31:44 +0000

    Fixes
    fixed many issues with loops not starting immediately
    
    ruined technical ability so changed some starting positions and audio files

--- music-assignment/music_game/Scenes/pitch_slider.gd
+++ music-assignment/music_game/Scenes/pitch_slider.gd
@@ -5,7 +5,6 @@ extends HSlider
 @onready var click_audio: AudioStreamPlayer = $"../click_audio"
 
 func _ready():
-	click_audio.pitch_scale = pitch_slider.value
 	pitch_slider.value_changed.connect(_on_slider_value_changed)
 
 func _on_slider_value_changed(value: float) -> void:
