1d72bc2ccdb79f5c3633f4d57d698f8e290533ac
y2kpartytime <C24732831@myTUDublin.ie>
2024-12-08 09:27:22 +0000

    Loops!!!
    -Added loops (individual + overall)
    
    -Added more sounds
    
    -Added more functions

--- music-assignment/music_game/Scripts/button_script.gd
+++ music-assignment/music_game/Scripts/button_script.gd
@@ -1,31 +1,29 @@
 extends Button
+@onready var reverb_bus = AudioServer.get_bus_index("ReverbBus")
+@onready var sfx_bus = AudioServer.get_bus_index("SFXBus")
+@onready var click_audio_player: AudioStreamPlayer = $click_audio 
+@onready var hover_audio_player: AudioStreamPlayer = $hover_audio  
 
-@onready var reverb_bus = AudioServer.get_bus_index("ReverbBus")  # ReverbBus (you can replace with the actual bus name)
-@onready var sfx_bus = AudioServer.get_bus_index("SFXBus")  # The other bus (SFXBus, for example)
-@onready var click_audio_player: AudioStreamPlayer = $click_audio  # AudioStreamPlayer for click sound
-@onready var hover_audio_player: AudioStreamPlayer = $hover_audio  # AudioStreamPlayer for hover sound
-@onready var reverb_box: CheckBox = $"../Button2/reverb_box"  # Reference to the CheckBox node
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-# Function to handle button press (click sound)
+#CLICK SOUND
 func _on_pressed() -> void:
 	if !click_audio_player.playing:
 		click_audio_player.play()
 		hover_audio_player.stop()
 
-# Function to handle mouse entering button (hover sound)
+#HOVER SOUND
 func _on_mouse_entered() -> void:
 	if !hover_audio_player.playing and !click_audio_player.playing:
 		hover_audio_player.play()
 
-# Function to swap the bus for the audio player based on checkbox state
+#MUTE
 func _on_check_box_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0,true)
 	click_audio_player.stop()
@@ -33,10 +31,12 @@ func _on_check_box_toggled(toggled_on: bool) -> void:
 	if !toggled_on:
 		AudioServer.set_bus_mute(0,false)
 
+#HOVER CODE
 func _on_check_box_mouse_entered() -> void:
 	if !hover_audio_player.playing and !click_audio_player.playing:
 		hover_audio_player.play()
 
+#WET
 func _on_check_box_2_toggled(toggled_on: bool) -> void:
 	var effect = AudioServer.get_bus_effect(3, 0)
 	effect.wet = 1
@@ -44,5 +44,13 @@ func _on_check_box_2_toggled(toggled_on: bool) -> void:
 	if !toggled_on:
 		effect.wet = 0
 
+#LOOP mp3
 func _on_check_box_3_toggled(toggled_on: bool) -> void:
-	pass
+	if toggled_on:
+		click_audio_player.stream.loop = true
+	else:
+		click_audio_player.stream.loop = false
+
+
+func _on_loop_box_toggled(toggled_on: bool) -> void:
+	_on_check_box_3_toggled(toggled_on)
